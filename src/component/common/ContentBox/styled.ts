9e6857d5adf21e6aba485f65898c1fd184d76fd1
hilim9 <dlagydls1212@naver.com>
2024-07-29 16:01:18 +0900

    검색창 위치 조정

--- src/component/common/ContentBox/styled.ts
+++ src/component/common/ContentBox/styled.ts
@@ -1,14 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const ContentBoxStyled = styled.div`
+    width: auto;
     background-color: #e0e0e0;
     padding: 10px 100% 10px 0px;
     border: 1px solid #ccc;
     box-sizing: border-box;
     display: block;
     + div {
-        margin-top: -52px;
-        margin-left: 450px;
+        margin-top: -61px;
+        margin-left: 700px;
     }
 `;
 
